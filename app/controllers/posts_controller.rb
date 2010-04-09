f4a27028090d377bdc02faccb562daf610373ba6
Rafael <ra.cintra@gmail.com>
2010-04-08 22:37:55 -0300

    form edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,24 @@ class PostsController < ApplicationController
     @posts = Post.order("created_at desc")
     respond_with @posts
   end  
+  
+  def new
+    @post = Post.new
+  end  
+  
+  def create
+    Post.create(params[:post])
+    respond_to posts_path
+  end  
+  
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+  
+  def update
+    post = Post.find_by_id(params[:id])
+    post.update_attributes(params[:post])
+    redirect_to posts_path
+  end
+    
 end
